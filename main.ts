13e45f19fd6a5943a15aa9f06ceb13de6cbd873d
dafeng306 <75168543+dafeng306@users.noreply.github.com>
2020-11-28 21:05:37 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,18 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        # # # # #
+        # . # . #
+        # . # . #
+        # . # . #
+        # . # . #
+        `)
+    basic.pause(200)
+    music.setVolume(255)
+    music.playMelody("C C E F G A F A ", 120)
+    basic.showIcon(IconNames.Rollerskate)
+    basic.showIcon(IconNames.Chessboard)
+    basic.showString("JOY")
+})
+basic.forever(function () {
+	
+})
