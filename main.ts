e95595f2aab4d9d6b7a7d4b9dc43bc864a87014f
dafeng306 <75168543+dafeng306@users.noreply.github.com>
2020-11-30 07:27:51 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -9,6 +9,7 @@ input.onButtonPressed(Button.A, function () {
     basic.pause(200)
     music.setVolume(255)
     music.playMelody("C C E F G A F A ", 120)
+    music.ringTone(262)
     basic.showIcon(IconNames.Rollerskate)
     basic.showIcon(IconNames.Chessboard)
     basic.showString("JOY")
